cdae9a3bf9d816d2d88a9c118e47cc23149b499c
mirazhasan47 <hasan3754@diu.edu.bd>
2023-03-27 23:49:16 +0600

    up

---
+++ api/routes/hotels.js
@@ -0,0 +1,29 @@
+import express from "express"
+import { countByCity, countByType, createHotel, deleteHotel, getHotel, getHotels, updateHotel } from "../controllers/hotel.js";
+import Hotel from "../models/Hotel.js"
+import createError from "../utils/error.js"
+import { verifyAdmin } from "../utils/verifyToken.js";
+
+const router = express.Router();
+
+//CREATE
+router.post("/", verifyAdmin, createHotel)
+
+//UPDATE
+
+router.put("/:id",verifyAdmin, updateHotel)
+
+//delete
+router.delete("/:id",verifyAdmin, deleteHotel)
+
+//get
+router.get("/find/:id", getHotel)
+
+//get all
+
+router.get("/", getHotels)
+
+router.get("/countByCity", countByCity)
+router.get("/countByType", countByType)
+
+export default router
